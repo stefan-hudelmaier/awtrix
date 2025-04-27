e4f93f86966a7bd9a604d3ba70707b3b7cf1cf1e
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2025-04-27 15:37:44 +0200

    Improve exception handling, do not crash

--- main.py
+++ main.py
@@ -65,36 +65,42 @@ def connect_mqtt():
 
     def on_message(client, userdata, msg):
 
-        global battery_charging
+        try:
+            global battery_charging
 
-        text = msg.payload.decode()
-        logger.debug(f"Received '{text}' from '{msg.topic}' topic")
-        app_name = None
-        icon = None
-        if msg.topic == "stefan/house/battery/level":
-            app_name = "solarbat"
-            icon = BATTERY_CHARGING_ICON if battery_charging else BATTERY_DISCHARGING_ICON
-        elif msg.topic == "stefan/house/inverters/total_dc_power":
-            app_name = "solarpower"
-            power = int(float(text))
-            text = str(power)
-            icon = SUN_ICON
-            battery_charging = power > 0
-        elif msg.topic == "finance/stock-exchange/index/GDAXI":
-            app_name = "dax"
-            icon = STOCK_ICON
-        elif msg.topic == "stefan/house/kpis/daily_pv_generation":
-            app_name = "dailypvgeneration"
-            icon = SOLAR_INPUT_ICON
-            text = str(int(float(text))) + " kWh"
-        elif msg.topic == "stefan/house/kpis/outside_temperature":
-            app_name = "outsidetemp"
-            icon = TEMPERATURE_ICON
-            text = str(int(float(text))) + "°C"
+            text = msg.payload.decode()
+            logger.debug(f"Received '{text}' from '{msg.topic}' topic")
+            app_name = None
+            icon = None
+            if msg.topic == "stefan/house/battery/level":
+                app_name = "solarbat"
+                icon = BATTERY_CHARGING_ICON if battery_charging else BATTERY_DISCHARGING_ICON
+            elif msg.topic == "stefan/house/inverters/total_dc_power":
+                app_name = "solarpower"
+                power = int(float(text))
+                text = str(power)
+                icon = SUN_ICON
+                battery_charging = power > 0
+            elif msg.topic == "finance/stock-exchange/index/GDAXI":
+                app_name = "dax"
+                icon = STOCK_ICON
+            elif msg.topic == "stefan/house/kpis/daily_pv_generation":
+                app_name = "dailypvgeneration"
+                icon = SOLAR_INPUT_ICON
+                text = str(int(float(text))) + " kWh"
+            elif msg.topic == "stefan/house/kpis/outside_temperature":
+                app_name = "outsidetemp"
+                icon = TEMPERATURE_ICON
+                text = str(int(float(text))) + "°C"
 
-        if app_name is not None and icon is not None:
-            requests.post(f"http://{awtrix_ip}/api/custom?name={app_name}", json={"text": text, "duration": 5, "icon": icon})
-            logger.info(f"Posted message to AWTRIX: {text}")
+            if app_name is not None and icon is not None:
+                try:
+                    requests.post(f"http://{awtrix_ip}/api/custom?name={app_name}", json={"text": text, "duration": 5, "icon": icon})
+                    logger.info(f"Posted message to AWTRIX: {text}")
+                except:
+                    logger.error("Error sending MQTT update to awtrix")
+        except:
+            logger.error("Error while processing MQTT message", exc_info=True)
 
     mqtt_client = mqtt.Client(client_id=client_id, callback_api_version=mqtt.CallbackAPIVersion.VERSION2, reconnect_on_failure=True)
     mqtt_client.tls_set(ca_certs='/etc/ssl/certs/ca-certificates.crt')
@@ -110,40 +116,52 @@ def connect_mqtt():
 
 def quotes():
     while True:
-        # quote = wikiquote.quotes('Hermann Hesse', lang='de')[0]
-        quote = wikiquote.quote_of_the_day(lang='de')
-        quote_text = f"{quote[0]} - {quote[1]}"
-        duration = len(quote) // 5 + 10
-        logger.info(f"Quote: {quote_text}")
-        requests.post(f"http://{awtrix_ip}/api/custom?name=quote", json={"text": quote_text, "duration": duration})
-        # TODO: Do this less often
-        sleep(5 * 60)
+        try:
+            # quote = wikiquote.quotes('Hermann Hesse', lang='de')[0]
+            quote = wikiquote.quote_of_the_day(lang='de')
+            quote_text = f"{quote[0]} - {quote[1]}"
+            duration = len(quote) // 5 + 10
+            logger.info(f"Quote: {quote_text}")
+            requests.post(f"http://{awtrix_ip}/api/custom?name=quote", json={"text": quote_text, "duration": duration})
+            # TODO: Do this less often
+            sleep(5 * 60)
+        except:
+            # Do not log stacktrace
+            # logger.exception("Error while sending quote")
+            logger.error("Error while sending quote")
+            sleep(30)
 
 
 def math_questions():
     while True:
+        try:
 
-        # Do this as a side effect
-        disable_standard_apps()
+            # Do this as a side effect
+            disable_standard_apps()
 
-        op = random.choice(['+', '-', '*'])
+            op = random.choice(['+', '-', '*'])
 
-        # For *
-        a = random.randint(2, 10)
-        b = random.randint(2, 10)
-
-        if op == '+':
-            a = random.randint(2, 50)
+            # For *
+            a = random.randint(2, 10)
             b = random.randint(2, 10)
 
-        if op == '-':
-            a = random.randint(15, 50)
-            b = random.randint(2, 14)
+            if op == '+':
+                a = random.randint(2, 50)
+                b = random.randint(2, 10)
 
-        text = f"{a} {op} {b}"
-        logger.info(f"Math question: {text}")
-        requests.post(f"http://{awtrix_ip}/api/custom?name=math", json={"text": text, "duration": 5, "icon": MATH_ICON})
-        sleep(30)
+            if op == '-':
+                a = random.randint(15, 50)
+                b = random.randint(2, 14)
+
+            text = f"{a} {op} {b}"
+            logger.info(f"Math question: {text}")
+            requests.post(f"http://{awtrix_ip}/api/custom?name=math", json={"text": text, "duration": 5, "icon": MATH_ICON})
+            sleep(30)
+        except:
+            # Do not log stacktrace
+            # logger.exception("Error while sending math question")
+            logger.error("Error while sending math question")
+            sleep(30)
 
 
 def english():
@@ -164,18 +182,24 @@ def english():
             translations.append((english, german))
 
     while True:
-        english, german = random.choice(translations)
-        text = f"{english} = {german}"
-        body = {
-            "text": [
-                {"t": english, "c": "#00FF00"},
-                {"t": " "},
-                {"t": german, "c": "#FF0000"}
-            ],
-            "duration": 15}
-        logger.info(f"English: {text}")
-        requests.post(f"http://{awtrix_ip}/api/custom?name=english", json=body)
-        sleep(30)
+        try: 
+            english, german = random.choice(translations)
+            text = f"{english} = {german}"
+            body = {
+                "text": [
+                    {"t": english, "c": "#00FF00"},
+                    {"t": " "},
+                    {"t": german, "c": "#FF0000"}
+                ],
+                "duration": 15}
+            logger.info(f"English: {text}")
+            requests.post(f"http://{awtrix_ip}/api/custom?name=english", json=body)
+            sleep(30)
+        except:
+            # Do not log stacktrace
+            # logger.exception("Error while sending English question")
+            logger.error("Error while sending English question")
+            sleep(30)
 
 
 def main():
