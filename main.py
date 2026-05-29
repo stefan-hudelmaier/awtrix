ae3bc9dc36c0f71f1569e31c297b431aba68f13e
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2026-05-29 11:37:13 +0200

    Fixing exception logging

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import json
-import paho.mqtt.client as mqtt
-import os
 import logging
+import os
+import random
 import sys
-import requests
 from threading import Thread
 from time import sleep
-import wikiquote
-import random
 
-broker = os.environ.get('MQTT_HOST', 'gcmb.io')
-client_id = os.environ['MQTT_CLIENT_ID']
-username = os.environ['MQTT_USERNAME']
-password = os.environ['MQTT_PASSWORD']
-awtrix_ip = os.environ['AWTRIX_IP']
+import paho.mqtt.client as mqtt
+import requests
+import wikiquote
+
+broker = os.environ.get("MQTT_HOST", "gcmb.io")
+client_id = os.environ["MQTT_CLIENT_ID"]
+username = os.environ["MQTT_USERNAME"]
+password = os.environ["MQTT_PASSWORD"]
+awtrix_ip = os.environ["AWTRIX_IP"]
 port = 8883
 
 STOCK_ICON = 52810
@@ -27,7 +29,7 @@ MATH_ICON = 5259
 SOLAR_INPUT_ICON = 37515
 TEMPERATURE_ICON = 2369
 
-log_level = os.environ.get('LOG_LEVEL', 'INFO')
+log_level = os.environ.get("LOG_LEVEL", "INFO")
 print("Using log level", log_level)
 
 logger = logging.getLogger()
@@ -35,7 +37,7 @@ logger.setLevel(log_level)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -74,7 +76,11 @@ def connect_mqtt():
             icon = None
             if msg.topic == "stefan/house/battery/level":
                 app_name = "solarbat"
-                icon = BATTERY_CHARGING_ICON if battery_charging else BATTERY_DISCHARGING_ICON
+                icon = (
+                    BATTERY_CHARGING_ICON
+                    if battery_charging
+                    else BATTERY_DISCHARGING_ICON
+                )
             elif msg.topic == "stefan/house/inverters/total_dc_power":
                 app_name = "solarpower"
                 power = int(float(text))
@@ -97,15 +103,22 @@ def connect_mqtt():
 
             if app_name is not None and icon is not None:
                 try:
-                    requests.post(f"http://{awtrix_ip}/api/custom?name={app_name}", json={"text": text, "duration": 5, "icon": icon})
+                    requests.post(
+                        f"http://{awtrix_ip}/api/custom?name={app_name}",
+                        json={"text": text, "duration": 5, "icon": icon},
+                    )
                     logger.info(f"Posted message to AWTRIX: {text}")
-                except:
-                    logger.error(f"Error sending MQTT update to awtrix. {}")
+                except Exception as e:
+                    logger.error(f"Error while handing or sending gcmb value: {e}")
         except:
             logger.error("Error while processing MQTT message", exc_info=True)
 
-    mqtt_client = mqtt.Client(client_id=client_id, callback_api_version=mqtt.CallbackAPIVersion.VERSION2, reconnect_on_failure=True)
-    mqtt_client.tls_set(ca_certs='/etc/ssl/certs/ca-certificates.crt')
+    mqtt_client = mqtt.Client(
+        client_id=client_id,
+        callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
+        reconnect_on_failure=True,
+    )
+    mqtt_client.tls_set(ca_certs="/etc/ssl/certs/ca-certificates.crt")
     mqtt_client.username_pw_set(username, password)
     mqtt_client.on_connect = on_connect
     mqtt_client.on_disconnect = on_disconnect
@@ -119,16 +132,18 @@ def connect_mqtt():
 def quotes():
     while True:
         try:
-
             # Do this as a side effect
             disable_standard_apps()
 
             # quote = wikiquote.quotes('Hermann Hesse', lang='de')[0]
-            quote = wikiquote.quote_of_the_day(lang='en')
+            quote = wikiquote.quote_of_the_day(lang="en")
             quote_text = f"{quote[0]} - {quote[1]}"
             duration = len(quote) // 2 + 15
             logger.info(f"Quote: {quote_text}")
-            r = requests.post(f"http://{awtrix_ip}/api/custom?name=quote", json={"text": quote_text, "duration": duration})
+            r = requests.post(
+                f"http://{awtrix_ip}/api/custom?name=quote",
+                json={"text": quote_text, "duration": duration},
+            )
             logger.info(f"Response from AWTRIX: {r.status_code} - {r.text}")
             # TODO: Do this less often
             sleep(5 * 60)
@@ -142,24 +157,26 @@ def quotes():
 def math_questions():
     while True:
         try:
-
-            op = random.choice(['+', '-', '*'])
+            op = random.choice(["+", "-", "*"])
 
             # For *
             a = random.randint(2, 10)
             b = random.randint(2, 10)
 
-            if op == '+':
+            if op == "+":
                 a = random.randint(2, 50)
                 b = random.randint(2, 10)
 
-            if op == '-':
+            if op == "-":
                 a = random.randint(15, 50)
                 b = random.randint(2, 14)
 
             text = f"{a} {op} {b}"
             logger.info(f"Math question: {text}")
-            requests.post(f"http://{awtrix_ip}/api/custom?name=math", json={"text": text, "duration": 5, "icon": MATH_ICON})
+            requests.post(
+                f"http://{awtrix_ip}/api/custom?name=math",
+                json={"text": text, "duration": 5, "icon": MATH_ICON},
+            )
             sleep(30)
         except:
             # Do not log stacktrace
@@ -186,16 +203,17 @@ def english():
             translations.append((english, german))
 
     while True:
-        try: 
+        try:
             english, german = random.choice(translations)
             text = f"{english} = {german}"
             body = {
                 "text": [
                     {"t": english, "c": "#00FF00"},
                     {"t": " "},
-                    {"t": german, "c": "#FF0000"}
+                    {"t": german, "c": "#FF0000"},
                 ],
-                "duration": 15}
+                "duration": 15,
+            }
             logger.info(f"English: {text}")
             requests.post(f"http://{awtrix_ip}/api/custom?name=english", json=body)
             sleep(30)
@@ -212,14 +230,14 @@ def main():
     quotes_thread = Thread(target=quotes, args=())
     quotes_thread.start()
 
-    #math_questions_thread = Thread(target=math_questions, args=())
-    #math_questions_thread.start()
+    # math_questions_thread = Thread(target=math_questions, args=())
+    # math_questions_thread.start()
 
-    #english_questions_thread = Thread(target=english, args=())
-    #english_questions_thread.start()
+    # english_questions_thread = Thread(target=english, args=())
+    # english_questions_thread.start()
 
     mqtt_client.loop_forever(retry_first_connection=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
